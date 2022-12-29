16aa2be68a6b0149e33af9a7affc497be1a04fa2
AshrafUzMahim <ashraf.uzzaman04082004@gmail.com>
2022-12-29 23:35:57 +0600

    29-dec-22 11:35

--- js/goToTop.js
+++ js/goToTop.js
@@ -1,7 +1,7 @@
 const goToTop = document.querySelector(".goToTop");
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 250) {
+  if (window.scrollY > 300) {
     goToTop.classList.add("active");
   } else {
     goToTop.classList.remove("active");
